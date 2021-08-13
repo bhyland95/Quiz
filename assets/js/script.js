ffa614ec05833d8728e8ac3c1c4493fecb02b7a0
Bhyland <blakehyland95@gmail.com>
2021-08-13 10:50:37 -0600

    Displays if you got the question right or wrong after answering for a short time. Cleaned up code a bit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//Arrays
 var quizQuestions = [
     { question: "What is the capital of Texas?", a: "Dallas", b: "Houston", c: "Austin", d: "San Antonio", answer: "Austin" },
     { question: "What is the capital of New York?", a: "New York City", b: "Albany", c: "Buffalo", d: "Syracuse", answer: "Albany" },
@@ -8,6 +9,7 @@ var quizQuestions = [
 
 var highscores = [];
 
+//Variables
 var question = document.querySelector('#question');
 var answer1 = document.querySelector('#answer1');
 var answer2 = document.querySelector('#answer2');
@@ -17,6 +19,7 @@ var nextBtn = document.querySelector('.submit-answer');
 var startBtn = document.querySelector('#start-btn');
 var saveBtn = document.querySelector('#save-btn');
 var quizContainer = document.querySelector('.quiz-container');
+var displayAnswer = quizContainer.querySelector('.player-answer')
 var instuctionContainer = document.querySelector('.instruction-container');
 var scoreContainer = document.querySelector('.score-container');
 var score = scoreContainer.querySelector('.score');
@@ -26,16 +29,23 @@ var playerScore = 0;
 
 //Start Button function 
 startBtn.onclick = function () {
+    //Hides instructions - Shows Quiz
     instuctionContainer.style.opacity = 0
     quizContainer.style.opacity = 1
     quizContainer.style.zIndex = 2
+    //Starts Timer
     timer;
+    //Displays initial question 
+    question.innerHTML = questionCounter + 1 + '. ' + quizQuestions[questionCounter].question
+    answer1.innerHTML = quizQuestions[questionCounter].a
+    answer2.innerHTML = quizQuestions[questionCounter].b
+    answer3.innerHTML = quizQuestions[questionCounter].c
+    answer4.innerHTML = quizQuestions[questionCounter].d
 }
 
 
 
-//Timer on the page @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-
+//Timer on the page
 var timer = setInterval(countdown, 1000);
 
 function countdown() {
@@ -46,15 +56,7 @@ function countdown() {
     if (time <= 0) {
         scoreScreen();
     }
-    }
-
-
-//On page load have questions and answer choices to first question.
-question.innerHTML = questionCounter + 1 + '. ' + quizQuestions[questionCounter].question
-answer1.innerHTML = quizQuestions[questionCounter].a
-answer2.innerHTML = quizQuestions[questionCounter].b
-answer3.innerHTML = quizQuestions[questionCounter].c
-answer4.innerHTML = quizQuestions[questionCounter].d
+}
 
 //WHen you click the submit button, checks answer, calls changeQuestion function
 nextBtn.onclick = function () {
@@ -66,7 +68,7 @@ nextBtn.onclick = function () {
         changeQuestion()
 
     } else {
-        //after last question -- display score div
+        //after pressing submit on last question -- display score div
         playerScore = time
         if (playerScore < 0) {
             playerScore = 0
@@ -92,11 +94,19 @@ var checkAnswer = function () {
         var UserAnswer = ""
     }
 
-    //Checks for Right Answer
+    //Checks User Answer againts the Right Answer
     if (UserAnswer == quizQuestions[questionCounter].answer) {
-        console.log("CORRECT ANSWER")
+        //Displays Correct for 1 second after answering if correct
+        displayAnswer.textContent = "CORRECT ANSWER"
+        setTimeout(function(){
+           displayAnswer.textContent = '';
+        }, 1000);
     } else {
-        console.log("WRONG ANSWER")
+        //Displays wrong for 1 second and deducts 10 seconds if wrong
+        displayAnswer.textContent = "WRONG ANSWER"
+        setTimeout(function(){
+            displayAnswer.textContent = '';
+         }, 1000);
         time = time - 10;
     }
 }
@@ -133,7 +143,7 @@ var scoreScreen = function () {
     score.textContent = playerScore;
 }
 
-//Function that loads info into quiz form. 
+//Function that loads new question and answer choices into quiz form. 
 var changeQuestion = function () {
     question.innerHTML = questionCounter + 1 + '. ' + quizQuestions[questionCounter].question
     answer1.innerHTML = quizQuestions[questionCounter].a
